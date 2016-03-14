5f031356b66fcd6c3dd12b8ae280bc39c2f32c62
mickl <michal.stefan.klusak@gmail.com>
2016-03-14 23:01:12 +0100

    Dodanie projektu z testami (xUnit)

---
+++ src/Byteville/Byteville.Tests/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Byteville.Tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Byteville.Tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Byteville.Tests")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("28e88520-7956-400f-a603-234a48d4a55b")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ src/Byteville/Byteville.Tests/ItemsControllerTests.fs
@@ -0,0 +1,10 @@
+﻿namespace Byteville.Tests
+open Xunit // import namespace
+
+
+type MyTest() = 
+    let add x y = x + y // unit
+
+    [<Fact>]   // test
+    let add_5_to_3_should_be_8() =
+        Assert.Equal(3, add 5 3)
