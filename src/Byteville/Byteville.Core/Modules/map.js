066811470bc94ddc93abb582d9def900a35e0beb
mickl <michal.stefan.klusak@gmail.com>
2016-03-24 20:04:45 +0100

    Kolorowanie mapy na podstawie wynikow agregacji

--- src/Byteville/Byteville.Core/Modules/map.js
+++ src/Byteville/Byteville.Core/Modules/map.js
@@ -1,9 +1,18 @@
-﻿export class Map{
-    constructor(){
+﻿import {HttpClient} from "aurelia-http-client"
 
+export class Map{
+    constructor(){
     }
     
-    activate(params){
-
+    activate(){
+        var client = new HttpClient();
+        var self = this;
+        client.get("api/districts").then( response => {
+            var response = JSON.parse(response.response);
+            for(var i = 0; i < response.length; i++){
+                var element = document.getElementById(response[i].name);
+                element.setAttribute("fill", `hsl(${8*i}, 100%, 50%)`);
+            }
+        })
     }
 }
