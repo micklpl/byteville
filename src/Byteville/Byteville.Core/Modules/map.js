9495bc382d1f90f2285bfb8ec29465029e83d759
mickl <michal.stefan.klusak@gmail.com>
2016-03-24 20:59:18 +0100

    Informacja o podświetlonej dzielnicy

--- src/Byteville/Byteville.Core/Modules/map.js
+++ src/Byteville/Byteville.Core/Modules/map.js
@@ -6,22 +6,29 @@ export class Map{
     
     activate(){
         var client = new HttpClient();
+        this.data = {};
+        this.details = undefined;
+
         var self = this;
         client.get("api/districts").then( response => {
             var response = JSON.parse(response.response);
             for(var i = 0; i < response.length; i++){
-                var element = document.getElementById(response[i].name);
+                var name = response[i].name;
+                var element = document.getElementById(name);
                 element.setAttribute("fill", `hsl(${8*i}, 100%, 50%)`);
+                this.data[name] = response[i].streetsCount;
             }
         })
     }
 
     selectDistrict($event){
         $event.srcElement.classList.add("pulsar-animation");
+        this.selectedItem  = $event.srcElement.id;
     }
 
     leaveDistrict($event){
         $event.srcElement.classList.remove("pulsar-animation");
+        this.selectedItem = undefined;
     }
 
 }
