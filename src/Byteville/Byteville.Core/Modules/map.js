da95825a958012274f10a7c9fe4162b310591b4f
mickl <michal.stefan.klusak@gmail.com>
2016-04-08 00:48:42 +0200

    Nawigacja po statystykach

--- src/Byteville/Byteville.Core/Modules/map.js
+++ src/Byteville/Byteville.Core/Modules/map.js
@@ -2,25 +2,79 @@
 
 export class Map{
     constructor(){
+        this.options = [
+            {name: "Liczba ogloszen"},
+            {field:"TotalPrice", name:"Cena"},
+            {field:"PricePerMeter", name:"Cena za metr"},
+            {field:"Area", name:"Powierzchnia"},
+            {field:"NumberOfRooms", name:"Liczba pokoi"},
+            {field:"YearOfConstruction", name:"Rok budowy"}            
+        ];
     }
     
-    activate(){
-        var client = new HttpClient();
+    activate(){        
         this.data = {};
         this.details = undefined;
 
         var self = this;
+        self.countItems(self);        
+    }
+
+    countItems(self){
+        var client = new HttpClient();
         client.get("api/aggregations/District").then( response => {
             var response = JSON.parse(response.response);
             for(var i = 0; i < response.length; i++){
                 var name = response[i].key;
                 var element = document.getElementById(name);
                 element.setAttribute("fill", `hsl(${8*i}, 100%, 50%)`);
-                this.data[name] = response[i].docCount;
+                self.data[name] = response[i].docCount;
             }
         })
+    }
 
+    statsAggregation(self, field){
+        var client = new HttpClient();
+        self.data = {};
+        client.get("api/aggregations/District?statsField=" + field).then( response => {
+            var response = JSON.parse(response.response);
+            response = response.sort((item1, item2) => {
+                let avg1 = item1.aggregations.inner_aggregation.average;
+                let avg2 = item2.aggregations.inner_aggregation.average;
+                if(avg1 === avg2)
+                    return 0;
+                if(avg1 < avg2)
+                    return 1;
+                return -1;
+            });
 
+            for(var i = 0; i < response.length; i++){
+                var name = response[i].key;
+                var element = document.getElementById(name);
+                element.setAttribute("fill", `hsl(${8*i}, 100%, 50%)`);
+            }
+
+            self.dataJSON = JSON.stringify(response.map(item => {
+                let stats = item.aggregations.inner_aggregation;
+                return{
+                    key: item.key,
+                    avg: stats.average,
+                    min: stats.min,
+                    max: stats.max
+                }
+            }));
+        })
+    }
+
+    executeAggregation(field){
+        var self = this;
+
+        if(field){
+            self.statsAggregation(self, field);
+        }
+        else{
+            self.countItems(self);
+        }
     }
 
     selectDistrict($event){
