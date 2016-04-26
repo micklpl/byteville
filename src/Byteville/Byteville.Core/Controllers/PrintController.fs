6ee4a5d0fc66cb553d130d9b9f72c82762b22ce6
mickl <michal.stefan.klusak@gmail.com>
2016-04-26 21:59:24 +0200

    Dodanie Edge.js do projektu

---
+++ src/Byteville/Byteville.Core/Controllers/PrintController.fs
@@ -0,0 +1,24 @@
+﻿namespace Byteville.Core.Controllers
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Net.Http
+open System.Web.Http
+open Newtonsoft.Json
+open EdgeJs
+
+type PrintController() =
+    inherit ApiController()
+
+    member x.Get() =
+        let func = Edge.Func("
+                return function (data, cb) {
+                    cb(null, 'Node.js ' + process.version + ' welcomes ' + data);
+                }
+            ")
+
+        let pr = func.Invoke(".NET")
+        let b = Async.AwaitTask pr |> Async.RunSynchronously
+
+        b
+   
