d29e87da33cb76f1cb3a83c2145c53383b8168c3
mickl <michal.stefan.klusak@gmail.com>
2016-05-03 23:30:26 +0200

    Metoda wektorow nosnych

--- src/Byteville/Byteville.Crawler/MachineLearning.fs
+++ src/Byteville/Byteville.Crawler/MachineLearning.fs
@@ -1,11 +1,14 @@
 ﻿module Byteville.MachineLearning
 open Accord.Statistics.Models.Regression.Linear
+open Accord.MachineLearning.VectorMachines.Learning
+open Accord.MachineLearning.VectorMachines
+open Accord.Statistics.Kernels
 open System
 
 let loadCsvData(path) = 
     System.IO.File.ReadAllLines(path) |> Seq.skip 1 //skip headers
 
-let findCoefficients(dataCsv:seq<string>) = 
+let parseCsvData(dataCsv:seq<string>) = 
     let dataCsv = dataCsv |> Seq.map(fun row -> row.Replace(".",","))
     let numData = dataCsv |> Seq.map(fun row -> 
                             (row.Split(';') |> Seq.map(System.Double.Parse)))
@@ -14,8 +17,29 @@ let findCoefficients(dataCsv:seq<string>) =
     let inputs = numData |> Seq.map(fun seq -> seq |> Seq.take(vectorDim) |> Array.ofSeq)
                          |> Array.ofSeq
     let outputs = numData |> Seq.map(fun seq -> seq |> Seq.last) |> Array.ofSeq
+    (vectorDim, inputs, outputs)
+
+let findLinearCoefficients(dataCsv:seq<string>) =     
+    let (vectorDim, inputs, outputs) = parseCsvData(dataCsv)
 
     let target = new MultipleLinearRegression(vectorDim)
     let error = target.Regress(inputs, outputs)
 
     target.Coefficients |> Seq.ofArray
+
+
+let createSvm(dataCsv:seq<string>) =
+    let (vectorDim, inputs, outputs) = parseCsvData(dataCsv)
+
+    //redukcja parametrow do minimum
+    let inputs = inputs |> Array.map(fun arr -> [|arr.[1]; arr.[5]|]) //l.pok, id.dziel
+    let vectorDim = 2
+
+    let machine = new KernelSupportVectorMachine(new Polynomial(3), vectorDim);
+    let learn = new SequentialMinimalOptimizationRegression(machine, inputs, outputs);
+
+    let error = learn.Run();
+
+    let testData = [|3.0; 12.0|]
+    let fxy = machine.Compute(testData);
+    machine
--- src/Byteville/Byteville.Tests/MachineLearningTests.fs
+++ src/Byteville/Byteville.Tests/MachineLearningTests.fs
@@ -9,6 +9,16 @@ type MachineLearningTests() =
         let path = @"C:\mydir\Projekty\ByteVIlle\src\DataStorage\adverts.csv"
         let data = Byteville.MachineLearning.loadCsvData(path) 
 
-        let coefficients = Byteville.MachineLearning.findCoefficients(data) |> Array.ofSeq
+        let coefficients = Byteville.MachineLearning.findLinearCoefficients(data) |> Array.ofSeq
 
         Assert.NotEmpty(coefficients)
+
+    [<Fact>]
+    member x.SvmTest() = 
+        let path = @"C:\mydir\Projekty\ByteVIlle\src\DataStorage\adverts.csv"
+        let data = Byteville.MachineLearning.loadCsvData(path) 
+
+        Byteville.MachineLearning.createSvm(data)
+
+        Assert.True(true)
+        
