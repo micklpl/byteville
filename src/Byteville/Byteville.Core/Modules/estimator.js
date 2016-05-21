9e3b7e514274725ab22844e8a66f0a985c5b44a1
mickl <michal.stefan.klusak@gmail.com>
2016-05-21 14:19:43 +0200

    Widok estymacji

--- src/Byteville/Byteville.Core/Modules/estimator.js
+++ src/Byteville/Byteville.Core/Modules/estimator.js
@@ -1,8 +1,35 @@
-﻿export class Estimator{
-    constructor(){
+﻿import {HttpClient} from "aurelia-http-client"
 
+export class Estimator{
+    constructor(){
+        this.district="";
+        this.area = "";
+        this.numberOfRooms = "";
+        this.tier = "";
+        this.parking = false;
+        this.yearOfConstruction = "";
+        this.estimatedPrice = undefined;
+        this.waitingForResults = false;
     }
     
     activate(){
+    }
+
+    executeEstimation(){
+        let client = new HttpClient();
+
+        let self = this;
+        let q = `?districtId=${this.district}&area=${this.area}&numberOfRooms=${this.numberOfRooms}
+                 &parking=${this.parking}&yearOfConstruction=${this.yearOfConstruction}`;
+        
+        this.waitingForResults = true;
+        client.get("api/estimations" + q).then( res => {
+            self.estimatedPrice = parseFloat(res.response).toFixed(2);
+            self.waitingForResults = false;
+        });
+    }
+
+    resetEstimationResult(){
+        this.estimatedPrice = undefined;
     }
 }
